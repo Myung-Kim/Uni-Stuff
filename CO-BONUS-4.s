760b655288250f33b6ad65498a629e35d542839b
Ricky-Kim <srh.forever@gmail.com>
2021-10-18 16:31:20 +0200

    tried to fix

--- CO-BONUS-4.s
+++ CO-BONUS-4.s
@@ -1,8 +1,3 @@
-.text
-	formatstr: .asciz "%c"
-	output: .asciz "\033[38;5;%ld;48;5;%ldm%c\033[0m"
-	soutput: .asciz "\033[6m%c\033[0m"
-
 .data
 	character: .quad 0x0000000000000000
 	amount:	.quad 0x0000000000000000
@@ -11,7 +6,13 @@
     fgcolor: .quad 0x0000000000000000
 
 .include "final.s"
+.text
+	formatstr: .asciz "%c"
+	output: .asciz "\033[38;5;%ld;48;5;%ldm%c"		# without special effect
+	soutput: .asciz "\033[%ldm%c"							# with effect
+	coutput: .asciz "\033[0m"
 
+#  to reset
 .global main
 
 # ************************************************************
@@ -29,6 +30,7 @@ decode:
 	# prologue
 	pushq	%rbp 			# push the base pointer (and align the stack)
 	movq	%rsp, %rbp		# copy stack pointer value to base pointer
+	movq 	%rdi, %r12		# move MESSAGE address to r12
 
 	# your code goes here
 start:
@@ -49,13 +51,52 @@ ploop:						# printing loop
 	cmpq $0, amount			# check how many times need to be printed
 	jle next				# jump to 'next' section to get next memory block position
 	movq bgcolor, %rdx		# move background color for compare??????
-	cmpq %rdx, fgcolor	# compare foreground and background color
+	cmpq %rdx, fgcolor		# compare foreground and background color
 	je addeffect			# if colors are equal, jump to add special effects
 	jmp noeffect			# jump to no effect printing section
 
 addeffect:
+	cmpq 	$0,   fgcolor		# reset to normal
+	je	eff_reset
+	cmpq 	$37,  fgcolor	# blink off
+	je eff_blink_off
+	cmpq 	$42,  fgcolor	# bold
+	je eff_bold
+	cmpq 	$66,  fgcolor	# faint
+	je eff_faint
+	cmpq	$105, fgcolor	# conceal
+	je eff_conceal
+	cmpq	$153, fgcolor	# reveal
+	je eff_reveal
+	cmpq	$182, fgcolor	# blink
+	je eff_blink
+
+
+eff_reset:
+	movq 	$0,   fgcolor
+	jmp	add_eff_end
+eff_blink_off:
+	movq 	$25,  fgcolor
+	jmp add_eff_end
+eff_bold:
+	movq	$1,   fgcolor
+	jmp add_eff_end
+eff_faint:
+	movq	$2,   fgcolor
+	jmp add_eff_end
+eff_conceal:
+	movq	$8,   fgcolor		# conceal $8
+	jmp add_eff_end
+eff_reveal:
+	movq	$28,  fgcolor		# reveal $28
+	jmp add_eff_end
+eff_blink:
+	movq	$5,   fgcolor	# fall through
+
+add_eff_end:
+	movq fgcolor, %rdx
 	movq character, %rsi
-	movq $soutput, %rdi
+	movq $soutput, %rdi		# format string for special effect
 	jmp callprint			# jump to skip no effect printing section
 
 noeffect:
@@ -74,7 +115,7 @@ next:
 
 	# movq $0, %rcx
 	movq position, %rcx
-	leaq MESSAGE(, %rcx, 8), %rdi	# get next address
+	leaq (%r12, %rcx, 8), %rdi	# get next address
 
 	jmp start
 
@@ -82,6 +123,7 @@ next:
 
 end:
 	# epilogue
+	popq 	%r12			# restore old r12
 	movq	%rbp, %rsp		# clear local variables from stack
 	popq	%rbp			# restore base pointer location
 	ret
@@ -92,6 +134,11 @@ main:
 
 	movq	$MESSAGE, %rdi	# first parameter: address of the message
 	call	decode			# call decode
+
+
+	movq $0, %rax			# no vector registers. (for printf)
+	movq $coutput, %rdi		# cancel any special effect at the end
+    call printf
 
 	popq	%rbp			# restore base pointer location
 	movq	$0, %rdi		# load program exit code
